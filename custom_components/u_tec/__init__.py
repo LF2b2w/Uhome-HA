d27e7dc672f6cceee67d464338e378cb88009892
Lf2b2w <wallis.luke4@gmail.com>
2025-03-07 00:07:20 +1100

    More breaks, more fixes

--- custom_components/u_tec/__init__.py
+++ custom_components/u_tec/__init__.py
@@ -8,18 +8,35 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client, config_entry_oauth2_flow
+from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET
 
 from . import api
-from .const import DOMAIN
+from .const import DOMAIN, OAUTH2_AUTHORIZE, OAUTH2_TOKEN
 from .coordinator import UhomeDataUpdateCoordinator
 
 _PLATFORMS: list[Platform] = [Platform.LOCK, Platform.LIGHT, Platform.SWITCH]
 
 # type UhomeConfigEntry = ConfigEntry[api.AsyncConfigEntryAuth]
 
+async def async_setup(hass: HomeAssistant, config):
+    """Set up the Uhome component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Uhome from a config entry."""
+    config_entry_oauth2_flow.register_oauth2_implementation(
+        hass,
+        DOMAIN,
+        config_entry_oauth2_flow.LocalOAuth2Implementation(
+            hass,
+            DOMAIN,
+            entry.data[CONF_CLIENT_ID],
+            entry.data[CONF_CLIENT_SECRET],
+            OAUTH2_AUTHORIZE,
+            OAUTH2_TOKEN,
+        ),
+    )
     implementation = (
         await config_entry_oauth2_flow.async_get_config_entry_implementation(
             hass, entry
